60561b38872d10b5487d6dd8007b003f745a88f2
Angel <87283923+angel-carv@users.noreply.github.com>
2026-08-29 05:31:31 +0000

    chapter1/mla01 Task1

--- chapter1/mla01/student/main.py
+++ chapter1/mla01/student/main.py
@@ -1,18 +1,21 @@
 def convert_kg(value):
+	kg_to_lb = value * 2.20462
+	kg_to_oz = value * 35.274
+
+	print(value , " kg converted is ", kg_to_lb, "pounds and ", kg_to_oz, "ounces.")
+
+#def convert_pounds(value):
 	# Your code starts here
 
 
-def convert_pounds(value):
-	# Your code starts here
-
-
-def convert_ounces(value):
+#def convert_ounces(value):
 	# Your code starts here
  
 
 if __name__ == "__main__":
+
 	# Test your code here
-    # convert_kg(10)
+	convert_kg(10)
     # convert_pounds(10)
     # convert_ounces(10)
 
