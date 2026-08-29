87285ba7b498b2b798d7c323463a75a9ebd4e845
Angel <87283923+angel-carv@users.noreply.github.com>
2026-08-29 03:30:14 +0000

    chapter1/ex03B All Tasks

--- chapter1/ex03B/student/main.py
+++ chapter1/ex03B/student/main.py
@@ -1,1 +1,10 @@
 # Write your code here
+PI = 3.14159
+radius = 3
+
+area = PI * radius**2
+diameter = radius * 2
+print("Diameter of the circle: ", diameter)
+print("Area of the circle: " ,area)
+
+
