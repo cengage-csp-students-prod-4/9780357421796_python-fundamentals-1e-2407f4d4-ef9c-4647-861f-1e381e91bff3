779d3cf15d1ffd69c134c7f2569127beaa65d619
Angel <87283923+angel-carv@users.noreply.github.com>
2026-08-29 03:08:34 +0000

    chapter1/ex03A Task1

--- chapter1/ex03A/student/main.py
+++ chapter1/ex03A/student/main.py
@@ -1,1 +1,15 @@
 # Write your code here
+distance_miles = 60
+time_hours = 3
+
+distance_knots = distance_miles / 1.15078
+distance_feet = distance_miles * 5280
+time_seconds = time_hours * 3600
+
+speed_knots = distance_knots / time_hours
+speed_mph = distance_miles / time_hours
+speed_fps = distance_feet / time_seconds
+
+print(speed_knots)
+print(speed_mph)
+print(speed_fps)
