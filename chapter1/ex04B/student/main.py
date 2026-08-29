9416555bd5975fee0f0cc0b58efc6d7deb1aadd9
Angel <87283923+angel-carv@users.noreply.github.com>
2026-08-29 03:51:33 +0000

    chapter1/ex04B All Tasks

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -1,1 +1,14 @@
 # Write your code here
+"""
+This is going to generate a multiplication
+table for the number that the user inputs :D
+"""
+whole_num = 7
+print("__________")
+print("Number:", whole_num)
+print("2:", whole_num * 2)
+print("4:", whole_num * 4)
+print("6:", whole_num * 6)
+print("8:", whole_num * 8)
+print("10:", whole_num * 10)
+print("__________")
