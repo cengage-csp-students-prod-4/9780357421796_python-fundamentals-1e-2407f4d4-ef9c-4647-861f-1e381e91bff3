d66754504940dc58c76a3a75afbcf5f7ab4e871a
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-03 22:52:49 +0000

    chapter2/ex01B All Tasks

--- chapter2/ex01B/student/main.py
+++ chapter2/ex01B/student/main.py
@@ -1,1 +1,11 @@
 # Write your code here
+days_driving = int(input("Days you have been driving:"))
+years_driving = days_driving // 365    
+days_of_year = days_driving % 365
+weeks_of_year = days_of_year // 7
+remaining_days = days_of_year % 7
+
+print("You have been driving for:")
+print("Years:", years_driving)
+print("Weeks:", weeks_of_year)
+print("Days:", remaining_days)
