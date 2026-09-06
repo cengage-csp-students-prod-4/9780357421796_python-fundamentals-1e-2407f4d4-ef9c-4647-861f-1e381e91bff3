3939a6ab68b9e32542d25096b96e4fc2b6a2f43e
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-06 21:22:33 +0000

    chapter2/ex02B All Tasks

--- chapter2/ex02B/student/main.py
+++ chapter2/ex02B/student/main.py
@@ -1,1 +1,9 @@
-# Write your code here
+word = input("What word do you want to convert? ")
+count = int(input("How many letters at the end should be uppercase? "))
+
+pivot = len(word) - count
+
+head = word[:pivot]
+tail = word[pivot:]
+
+print(head + tail.upper())
