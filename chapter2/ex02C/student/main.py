377c6c5db81983676b9ccf1c3d00693a2658a3b1
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-09 01:01:05 +0000

    chapter2/ex02C Task1

--- chapter2/ex02C/student/main.py
+++ chapter2/ex02C/student/main.py
@@ -6,5 +6,5 @@ clean_string = str.strip(str.lower(user_input_string))
 
 occurrences = clean_string.count(word_to_look_for)
 
-print(f"There are {occurrences} occurances of '{word_to_look_for}' in the sentence.")
+print(f"There are {occurrences} occurrences of '{word_to_look_for}' in the sentence.")
 
