164d51abbc799bdfdc229be341b96c99524e6cf1
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-09 00:52:40 +0000

    chapter2/ex02C All Tasks

--- chapter2/ex02C/student/main.py
+++ chapter2/ex02C/student/main.py
@@ -1,1 +1,10 @@
-# Write your code here
+user_input_string = input("Sentence: ")
+word_to_look_for = input("Word to look for in sentence: ")
+
+clean_string = str.strip(str.lower(user_input_string))
+
+
+occurances = clean_string.count(word_to_look_for)
+
+print(f"There are {occurances} of {word_to_look_for} in the sentence")
+
