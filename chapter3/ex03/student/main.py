0354da9b7135faf916bbe36d1572d05a3010d22c
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-11 01:29:37 +0000

    chapter3/ex03 All Tasks

--- chapter3/ex03/student/main.py
+++ chapter3/ex03/student/main.py
@@ -1,1 +1,14 @@
-# Write your code here
+name = input("Enter you name: ")
+password = input("Enter your password: ")
+
+real_password = "Pas$Word" 
+
+if(password == real_password):
+    print(f"Welcome back, {name}")
+
+
+while(password != real_password):
+    print("Incorrect password, try again...")
+    password = input("Enter your password: ")
+    if(password == real_password):
+        print(f"Welcome back, {name}")
