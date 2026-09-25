77cb80e4b3a5a02e76c8e44311792975531e7c6b
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-25 01:08:22 +0000

    chapter6/ex01B All Tasks

--- chapter6/ex01B/student/main.py
+++ chapter6/ex01B/student/main.py
@@ -1,1 +1,13 @@
-# Write your code here
+def word_counter(word):
+
+    occurances = dict()
+
+    for char in word:
+        if(char in occurances):
+            occurances[char] += 1
+        else:
+            occurances[char] = 1
+
+    return occurances
+
+print(word_counter("Mississippi")) 
