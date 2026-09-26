6815cea5ce2016812771a086af24422b3c25d092
Angel <87283923+angel-carv@users.noreply.github.com>
2026-09-26 06:12:28 +0000

    chapter6/ex01C Task1

--- chapter6/ex01C/student/main.py
+++ chapter6/ex01C/student/main.py
@@ -1,1 +1,11 @@
-# Write your code here
+def content_combiner(dic1, dic2):
+    new_dic = dict()
+    for key, value in dic1.items():
+        new_dic[key] = value
+
+    for key, value in dic2.items():
+        new_dic[key] = value
+
+    return new_dic
+
+
